273a1e30121321a11bc04a2fd4407f95e730e1c7
Luciene Araujo <202601269232@alunos.estacio.br>
2026-02-02 03:51:08 +0000

    Código corrigido

--- Supertrunfo.c
+++ Supertrunfo.c
@@ -1,50 +1,61 @@
 #include <stdio.h>
 
 int main () {
-//1° carta cadastrada    
-    int(carta01);
-        char carta1[20]  = "Carta 01" ;
-        char estado[20] = "São_Paulo";
-        char codigo[20] = "A01";
-        char cidade[20] = "São_Paulo";
-        int populacao = 12325000;
-        float area = 1521.11;
-        float pib = 699.28;
-        int turistas = 50; 
-            printf("Escolha uma carta de 1 a 2: ");
-            scanf("\nCarta 01%s", &carta1);
-            printf("%s\n", carta1);
-            printf("Estado: %s\n", &estado);
-            printf("Código: %s\n", &codigo);
-            printf("Cidade: %s\n", &cidade);
-            printf("População: %d\n", &populacao);
-            printf("Área: %f km²\n", &area);
-            printf("PIB: %f bilhões\n", &pib);
-            printf("Número de pontos turísticos: %d\n\n", &turistas);
-//limpa teclado    
-getchar();
-//2° carta cadastrada
-    int(carta02);
-        char carta2[20]  = "Carta 02" ;
-        char estado02[20] = "Rio_de_Janeiro";
-        char codigo02[20] = "A02";
-        char cidade02[20] = "Rio_de_Janeiro";
-        int populacao02 = 6748000;
-        float area02 = 1200.25;
-        float pib02 = 300.50;
-        int turistas02 = 30;
-            printf("Escolha uma carta de 1 a 2: ");
-            scanf("\nCarta 02%s", &carta2);
-            printf("%s\n", carta2);
-            printf("Estado: %s\n", &estado02);
-            printf("Código: %s\n", &codigo02);
-            printf("Cidade: %s\n", &cidade02);
-            printf("População: %d\n", &populacao02);
-            printf("Área: %f km²\n", &area02);
-            printf("PIB: %f bilhões\n", &pib02);
-            printf("Número de pontos turísticos: %d\n\n", &turistas02);
 
+    int escolha;
 
+//Carta 01    
+    char carta1[]  = "Carta 01" ;
+    char estado1[] = "São_Paulo";
+    char codigo1[] = "A01";
+    char cidade1[] = "São_Paulo";
+    int populacao1 = 12325000;
+    float area1 = 1521.11;
+    float pib1 = 699.28;
+    int turistas1 = 50; 
+            
+//Carta 02
+    char carta2[]  = "Carta 02" ;
+    char estado2[] = "Rio_de_Janeiro";
+    char codigo2[] = "A02";
+    char cidade2[] = "Rio_de_Janeiro";
+    int populacao2 = 6748000;
+    float area2 = 1200.25;
+    float pib2 = 300.50;
+    int turistas2 = 30;
+
+//Selecionar a carta
+    printf("Escolha uma carta de 1 a 2: ");
+    scanf("%d", &escolha);
+
+//Se a carta escolhida foi a 1
+    if(escolha == 1) {
+        printf("\n%s\n", carta1);
+        printf("Estado: %s\n", estado1);
+        printf("Código: %s\n", codigo1);
+        printf("Cidade: %s\n", cidade1);
+        printf("População: %d\n", populacao1);
+        printf("Área: %.2f km²\n", area1);
+        printf("PIB: %.2f bilhões\n", pib1);
+        printf("Número de pontos turísticos: %d\n\n", turistas1);
+    }
+
+//Se a cata escolhida foi a 2
+   else if (escolha == 2) {
+        printf("\n%s\n", carta2);
+        printf("Estado: %s\n", estado2);
+        printf("Código: %s\n", codigo2);
+        printf("Cidade: %s\n", cidade2);
+        printf("População: %d\n", populacao2);
+        printf("Área: %.2f km²\n", area2);
+        printf("PIB: %.2f bilhões\n", pib2);
+        printf("Número de pontos turísticos: %d\n\n", turistas2);
+    }
+
+//Se nenhuma das cartas foi escolhida
+    else {
+        printf("\nOpção inválida!\n\n");
+    }
 
 return 0;
 
